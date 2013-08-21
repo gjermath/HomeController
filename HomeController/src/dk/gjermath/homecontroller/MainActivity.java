fec80567de2a3f594459378455dee2a647231c7e
gjermath <thomas@gjermansen.dk>
2013-08-21 20:42:47 +0200

    .

--- HomeController/src/dk/gjermath/homecontroller/MainActivity.java
+++ HomeController/src/dk/gjermath/homecontroller/MainActivity.java
@@ -2,7 +2,9 @@ package dk.gjermath.homecontroller;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,17 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.action_settings:
+                Intent settingsActivity = new Intent(getBaseContext(), SettingsActivity.class);
+                startActivity(settingsActivity);
+                return true;
+        default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
---
+++ HomeController/src/dk/gjermath/homecontroller/SettingsActivity.java
@@ -0,0 +1,13 @@
+package dk.gjermath.homecontroller;
+
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+	@SuppressWarnings("deprecation")
+	public SettingsActivity() {
+		// TODO Auto-generated constructor stub
+        addPreferencesFromResource(R.xml.preferences);
+	}
+
+}
